5e7418bcadceea3febd2655be846954201e652f1
gusrb8925 <64997245+gusrb8925@users.noreply.github.com>
2020-11-28 15:18:53 +0900

    Add files via upload

---
+++ code-assignment2/src/index.js
@@ -0,0 +1,17 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+const title = document.querySelector("body");
+
+function hadlerResize() {
+  const width = window.innerWidth;
+  if (width < 500) {
+    title.classList.add("small");
+  } else if (width > 700) {
+    title.classList.add("large");
+  } else {
+    title.className = "";
+  }
+}
+
+window.addEventListener("resize", hadlerResize);
